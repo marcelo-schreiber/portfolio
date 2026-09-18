a24212615d2d6610cc988f960350176bd8a2b678
marcelo-schreiber <marcelorissette15@gmail.com>
2026-09-18 18:35:45 -0300

    Migrate all to latest dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig, sharpImageService } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
   image: {
     service: sharpImageService(),
   },
-  integrations: [tailwind(), react()],
+  integrations: [react()],
 });
